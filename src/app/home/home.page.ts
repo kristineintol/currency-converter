888a07feadd44708c1d8253e939321d7a079f387
kristineintol <kristineintol@gmail.com>
2023-05-31 16:43:45 +0800

    converter

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+interface Currency {
+  code: string;
+  name: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +11,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  amount!: number;
+  fromCurrency!: string;
+  toCurrency!: string;
+  convertedAmount!: number;
+  currencies: Currency[] = [
+    { code: 'USD', name: 'United States Dollar' },
+    { code: 'EUR', name: 'Euro' },
+    { code: 'GBP', name: 'British Pound Sterling' },
+    { code: 'JPY', name: 'Japanese Yen' },
+    { code: 'AUD', name: 'Australian Dollar' }
+  ];
+  conversionRateAPI = 'https://api.exchangerate-api.com/v4/latest/'; // Replace with your preferred currency conversion API
+
+  constructor(private http: HttpClient) {}
+
+  convertCurrency() {
+    if (this.amount && this.fromCurrency && this.toCurrency) {
+      const url = `${this.conversionRateAPI}${this.fromCurrency}`;
+      this.http.get<any>(url).subscribe((data) => {
+        if (data.rates && data.rates[this.toCurrency]) {
+          const rate = data.rates[this.toCurrency];
+          this.convertedAmount = this.amount * rate;
+        }
+      });
+    }
+  }
 
 }
